37a2511f44facedccce2f77a49ed432cfd58dea2
minhkl <lkminh.uns@gmail.com>
2022-02-13 22:40:18 +0700

    Basic config: next.config.js, tsconfig.json, eslint, prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  baseUrl: "src",
+  pageExtensions: ["page.tsx"],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
+};
